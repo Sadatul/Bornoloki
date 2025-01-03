73c411bbf59a09df8c2a151c3da6561458f639ca
sadatul <sadatulislamsadi@gmail.com>
2025-01-03 12:58:16 +0600

    Fixed: Merge error

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,15 +5,12 @@ class Settings(BaseSettings):
     db_url: str
     redis_host: str
     redis_port: int
-<<<<<<< HEAD
     supabase_jwt_public_key: str
     supabase_url: str
     supabase_service_role_key: str
     
-=======
     openai_api_key: str  # <-- Added field
 
->>>>>>> 6d1678b8f7ea8f061329e1e694aa52c5a7e4fda5
     model_config = SettingsConfigDict(env_file=".env")
 
 
